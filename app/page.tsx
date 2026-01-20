44223513777fa509ac3a26ced90e9397fe9ebd15
andersonato <andersonalencartoscano@gmail.com>
2026-01-20 14:02:34 +0000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>UPTOVAI</h1>
+      <p>Template base de landing page funcionando.</p>
+    </main>
+  );
+}
